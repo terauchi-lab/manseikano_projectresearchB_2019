a57b1c3526083c00da5096a8963ed28f17a066d1
mansei <manseikano@gmail.com>
2019-11-27 18:18:34 +0900

    テストファイルを移動

--- test/Test1.java
+++ test/Test1.java
@@ -1,17 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-public class Test {
+public class Test1 {
   public static void main(String[] args){
+    //変数宣言
     int a;
+    //代入
     a = 100;
+
+    //初期化
     int b = 3, c = 1;
 
-    float f;
-    f = 3.04f;
+    float f = 3.04f;
 
-    char ch;
-    ch = 'k';
+    char ch = 'k';
 
     double d,e;
     d = e = 3.01;
---
+++ test/Test1_err.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Test1_err {
+  public static void main(String[] args){
+    //変数宣言
+    int a;
+
+    //代入
+    a = 100.0;
+
+    //重複した変数宣言
+    double a = 100.0;
+
+    //初期化
+    int b = 3, c = true;
+
+    float f = 3.04;
+
+    char ch = "k";
+  }
+}
