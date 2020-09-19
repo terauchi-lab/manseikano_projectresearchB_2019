129b6e8949a706fb5a9993dfb7d4607cee111ef9
mansei <manseikano@gmail.com>
2020-09-19 21:22:25 +0900

    refactoring code

--- test/Test2.java
+++ test/Test2.java
@@ -1,7 +1,13 @@
-class A {}
+class A {
+  /*@ Ref p @*/ A(){}
+  /*@ [p; p->{c:A}] @*/
+}
 
 class B extends A {
   A x;
+
+  /*@ Ref p @*/ B(){}
+  /*@ [p; p->{c:B, x:NULL}] @*/
 
   /*@ [pt; pt->{c:B, x:NULL}] @*/
   /*@ Ref pt@*/ A m(){
@@ -10,10 +16,13 @@ class B extends A {
   /*@ [p; pt->{c:B,x:Ref p}, p->{c:B, x:NULL}] @*/
 }
 
-class C extends A{}
+class C extends A{
+  /*@ Ref p @*/ C(){}
+  /*@ [p; p->{c:C}] @*/
+}
 
 class Test2 {
-  void main(){
+  public static void main(String[] args){
     B b1 = new B()/*@[][p1]@*/;
     b1.m()/*@[p1][p2]@*/;
     b1.x = new C()/*@[][p3]@*/;
